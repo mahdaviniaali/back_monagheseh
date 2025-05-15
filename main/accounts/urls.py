fe067ebead451685a80d4ed6aa140222a5c2d4c9
unknown <alimahdavinia125@gmail.com>
2025-05-15 19:15:08 +0330

    firs commit 01

---
+++ main/accounts/urls.py
@@ -0,0 +1,8 @@
+﻿from django.urls import path
+from . import views
+
+app_name = 'accounts'
+
+urlpatterns = [
+   
+]
---
+++ main/admin_panel/urls.py
@@ -0,0 +1,7 @@
+﻿from django.urls import path
+from . import views
+
+app_name = 'admin_panel'
+
+urlpatterns = [
+]
